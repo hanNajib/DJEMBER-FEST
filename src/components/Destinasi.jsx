aac1e32748cc566b9d9aee7f64fbae5c8e5e90ca
hanNajib <arroyhan27@gmail.com>
2024-10-12 22:09:04 +0700

    destinasi

---
+++ src/components/Destinasi.jsx
@@ -0,0 +1,35 @@
+import { useInView } from "framer-motion"
+import { useRef } from "react"
+
+export default function DestinasiKanan() {
+    const refAtas = useRef(null)
+    const isInView = useInView(refAtas)
+    return(
+      <>
+        <div ref={refAtas} className="bg-gray-100 py-12 flex flex-col md:flex-row items-center">
+        <div className="container mx-auto flex flex-col md:flex-row items-start">
+          <div className="w-full md:w-1/2 text-left relative">
+            <h2
+            className="text-[4rem] md:text-[7rem] font-bebas_neue text-gray-800 absolute top-0" style={{ transform: isInView ? "none" : "translateX(-200px)", opacity: isInView ? 1 : 0, transition: "all .8s"}}>
+              Destinasi Jember
+            </h2>
+
+            <div
+            className="mt-16 md:mt-36">
+              <h2 className="text-2xl md:text-3xl font-bold text-gray-800" style={{ transform: isInView ? "none" : "translateX(-200px)", opacity: isInView ? 1 : 0, transition: "all .8s"}}>Papuma Beach</h2>
+              <p className="text-gray-600 mt-4 max-w-2xl" style={{ transform: isInView ? "none" : "translateX(-200px)", opacity: isInView ? 1 : 0, transition: "all .8s"}}>
+                Pantai Papuma merupakan Kabupaten yang terletak di ujung timur Pulau Jawa yang memiliki keragaman suku budaya, 
+                serta keindahan alam yang luar biasa. Berbagai wisata alam kami suguhkan untuk menemani liburan anda sekalian.
+              </p>
+              <p className="text-gray-800 mt-6 font-semibold" style={{ transform: isInView ? "none" : "translateX(-200px)", opacity: isInView ? 1 : 0, transition: "all .8s"}}>Enjoy Your Holiday...</p>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/2">
+            <img src="image/gallery/papuma1.jpg" alt="Pantai Papuma" className="ml-auto object-cover w-full h-auto" style={{ transform: isInView ? "none" : "translateX(200px)", opacity: isInView ? 1 : 0, transition: "all .8s"}}/>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+}
