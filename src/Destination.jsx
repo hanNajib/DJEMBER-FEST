22c4275b7e9be033384a97f9661b46b3b5c16652
RAFIF-RF10 <rafifgithub24@gmail.com>
2024-10-16 21:58:59 +0700

    logout

--- src/Destination.jsx
+++ src/Destination.jsx
@@ -70,26 +70,39 @@ export default function Destination() {
 
   return (
     <>
-      <div className="bg-neutral-800 overflow-x-hidden" id="home">
-        <section className="home w-[100vw] h-[100vh]" id="">
-          <div
-            className="absolute w-full h-[100vh] inset-0 bg-cover bg-right bg-fixed opacity-70"
-            style={{
-              backgroundImage: `url('image/gallery/bg-destinasi.jpg')`,
-              backgroundBlendMode: "multiply",
-            }}
-          ></div>
+    <div className="bg-neutral-800 overflow-x-hidden" id="home">
+  <section className="home w-[100vw] h-[100vh]" id="">
+    <div
+      className="absolute w-full h-[100vh] inset-0 bg-cover bg-right bg-fixed opacity-70"
+      style={{
+        backgroundImage: `url('image/gallery/bg-destinasi.jpg')`,
+        backgroundBlendMode: "multiply",
+      }}
+    ></div>
 
-          <div className="absolute w-full h-[105vh] bg-fixed"></div>
-          <div className="w-[100%] h-full flex items-center justify-center relative">
-            <div className="text flex flex-col">
-              <p className="sh judul text-[3rem] md:text-[4rem] lg:text-[10rem] text-slate-50 text-center font-bebas_neue font-semibold">
-                Destination Djember
-              </p>
-            </div>
-          </div>
-        </section>
+    <div className="absolute w-full h-[105vh] bg-fixed"></div>
+
+    {/* SVG Icon di pojok kiri atas */}
+    <a href="" className="absolute top-4 left-4 transform rotate-180">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        className="w-12 h-12 text-white" // Atur ukuran dan warna sesuai kebutuhan
+      >
+        <path fill="#FFFF" d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+      </svg>
+    </a>
+
+    <div className="w-[100%] h-full flex items-center justify-center relative">
+      <div className="text flex flex-col">
+        <p className="sh judul text-[3rem] md:text-[4rem] lg:text-[10rem] text-slate-50 text-center font-bebas_neue font-semibold">
+          Destination Djember
+        </p>
       </div>
+    </div>
+  </section>
+</div>
+
 
       {/* Search bar */}
       <div className="flex justify-center px-4 py-10 md:px-20 md:py-20">
